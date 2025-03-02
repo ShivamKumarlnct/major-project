f4cb04ba4d99f4f6315588e77f142af7ebc053dd
ShivamKumarlnct <shivamsitu2@gmail.com>
2025-03-02 22:32:43 +0530

    update review

--- routes/review.js
+++ routes/review.js
@@ -4,12 +4,14 @@ const wrapAsync = require("../utils/wrapfunc.js");
 const Expresserror = require("../utils/Expresserror.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const {validateReview} = require("../middleware.js");
+const {validateReview, isowner, isLoggedIn} = require("../middleware.js");
 const reviewController=require("../controller/review.js");
 
 
 // create Review
-router.post("/", validateReview, wrapAsync(reviewController.createReview));
+router.post("/",isLoggedIn,
+     validateReview, 
+     wrapAsync(reviewController.createReview));
 
 // Delete Review
 router.delete("/:reviewId", wrapAsync(reviewController.deleteReview));
